974a607f00def94aac5780401c6cdb57161089c9
dodobby <142660660+dodobby@users.noreply.github.com>
2025-03-20 09:46:58 +0900

    Update main.tf

--- aws-eks-fargate/main.tf
+++ aws-eks-fargate/main.tf
@@ -12,6 +12,7 @@ module "eks" {
   cluster_name                   = var.cluster_name
   cluster_version                = var.kubernetes_version 
   cluster_endpoint_public_access = true
+  create_iam_role = true
 
   vpc_id     = var.vpc_id
   subnet_ids = var.private_subnets
