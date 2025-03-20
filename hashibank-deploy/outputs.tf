920c07dea714e3d70c827da97ea38786309cf88a
dodobby <142660660+dodobby@users.noreply.github.com>
2025-03-20 18:26:04 +0900

    Update outputs.tf

--- hashibank-deploy/outputs.tf
+++ hashibank-deploy/outputs.tf
@@ -2,14 +2,6 @@ locals {
   lb_name = kubernetes_ingress_v1.hashibank.status[0].load_balancer[0].ingress[0].hostname
 }
 
-data "aws_elb" "hashibank" {
-  name = local.lb_name
-}
-
 output "load_balancer_name" {
   value = local.lb_name
-}
-
-output "load_balancer_info" {
-  value = data.aws_elb.hashibank
 }
